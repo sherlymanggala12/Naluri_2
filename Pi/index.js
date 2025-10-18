2ffc585e5d51053838e2db7f24c358877b9bb330
Sherly Manggala <v.sherly.manggala@adira.co.id>
2025-10-18 13:17:40 +0700

    Add formula to calculate Pi

--- Pi/index.js
+++ Pi/index.js
@@ -1,8 +1,32 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = 3000;
 
+let iterations = 0;
+
+// Using Nilakantha formula to calculate Pi
+function calculatePi(iterations) {
+	let pi = 3;
+	let x = 1;
+
+	for (let n = 2; n <= iterations * 2; n += 2) {
+		const temp = (4 / (n * (n + 1) * (n + 2))) * x;
+		pi += temp;
+		x = -x;
+	}
+	return pi;
+}
+
+app.get("/pi", async (req, res) => {
+	iterations += 1000; // increase for next precision
+	const pi = calculatePi(iterations);
+
+	res.json({
+		pi: pi.toString(),
+	});
+});
+
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+	console.log(`Server running at http://localhost:${port}`);
 });
