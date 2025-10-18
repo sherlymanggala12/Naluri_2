65630348e0f5fb9d61a2096eafc40f5fb7556389
Sherly Manggala <v.sherly.manggala@adira.co.id>
2025-10-18 15:43:18 +0700

    add function to get the calculated Pi from the server with axios

--- App/app/index.tsx
+++ App/app/index.tsx
@@ -1,6 +1,25 @@
-import {Text, View, ViewStyle} from "react-native";
+import {useState, useEffect} from "react";
+import {
+	Text,
+	View,
+	ViewStyle,
+	TextStyle
+} from "react-native";
+import axios from "axios";
 
 export default function Index() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/pi');
+        console.log("Fetched data:", response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, []);
+
 	return (
 		<View style={$container}>
 			<Text style={$text}>Did you know that our sun's circumference is</Text>
@@ -16,6 +35,6 @@ const $container: ViewStyle = {
 };
 
 const $text: TextStyle = {
-  fontFamily: "'Nunito Sans', sans-serif",
-  color: "#ffffff",
+	fontFamily: "'Nunito Sans', sans-serif",
+	color: "#ffffff",
 };
