412389725f15f5738e4404e08de33338ba15c294
Sherly Manggala <v.sherly.manggala@adira.co.id>
2025-10-18 13:22:16 +0700

    add new library to display more decimals for better precision

--- Pi/index.js
+++ Pi/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { createClient } = require("redis");
+const Decimal = require("decimal.js");
 
 const app = express();
 const port = 3000;
@@ -8,26 +9,28 @@ const port = 3000;
 const redis = createClient({ url: "redis://127.0.0.1:6379" }); // Redis port 6379
 redis.on("error", (err) => console.error("Redis error:", err));
 
-let iterations = 0;
-
 (async () => {
   await redis.connect();
   console.log("Redis connected");
 
   // Using Nilakantha formula to calculate Pi
   function calculatePi(iterations) {
-    let pi = 3;
+    Decimal.set({ precision: 200 });
+    let pi = new Decimal(3);
     let x = 1;
 
     for (let n = 2; n <= iterations * 2; n += 2) {
-      const temp = (4 / (n * (n + 1) * (n + 2))) * x;
-      pi += temp;
+      const temp = new Decimal(4)
+        .div(new Decimal(n).times(n + 1).times(n + 2))
+        .times(x);
+      pi = pi.plus(temp);
       x = -x;
     }
     return pi;
   }
 
   app.get("/pi", async (req, res) => {
+    let iterations = parseInt((await redis.get("iterations")) || "0", 10);
     iterations += 1000; // increase for next precision
     const pi = calculatePi(iterations);
 
