59bd56033780f9d88673a51e16d6d1f531953a22
Sherly Manggala <v.sherly.manggala@adira.co.id>
2025-10-18 15:20:01 +0700

    separate text style

--- App/app/index.tsx
+++ App/app/index.tsx
@@ -3,7 +3,7 @@ import {Text, View, ViewStyle} from "react-native";
 export default function Index() {
 	return (
 		<View style={$container}>
-			<Text style={{color: "#ffffff"}}>Test</Text>
+			<Text style={$text}>Did you know that our sun's circumference is</Text>
 		</View>
 	);
 }
@@ -13,4 +13,9 @@ const $container: ViewStyle = {
 	justifyContent: "center",
 	alignItems: "center",
 	backgroundColor: "#00111a",
+};
+
+const $text: TextStyle = {
+  fontFamily: "'Nunito Sans', sans-serif",
+  color: "#ffffff",
 };
