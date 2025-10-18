3510fc1b5767b3bf0fec7f015d65fe0a83bc0881
Sherly Manggala <v.sherly.manggala@adira.co.id>
2025-10-18 13:20:25 +0700

    add redis

--- Pi/index.js
+++ Pi/index.js
@@ -1,32 +1,44 @@
 const express = require("express");
+const { createClient } = require("redis");
 
 const app = express();
 const port = 3000;
 
+// Connect to Redis
+const redis = createClient({ url: "redis://127.0.0.1:6379" }); // Redis port 6379
+redis.on("error", (err) => console.error("Redis error:", err));
+
 let iterations = 0;
 
-// Using Nilakantha formula to calculate Pi
-function calculatePi(iterations) {
-	let pi = 3;
-	let x = 1;
+(async () => {
+  await redis.connect();
+  console.log("Redis connected");
 
-	for (let n = 2; n <= iterations * 2; n += 2) {
-		const temp = (4 / (n * (n + 1) * (n + 2))) * x;
-		pi += temp;
-		x = -x;
-	}
-	return pi;
-}
+  // Using Nilakantha formula to calculate Pi
+  function calculatePi(iterations) {
+    let pi = 3;
+    let x = 1;
 
-app.get("/pi", async (req, res) => {
-	iterations += 1000; // increase for next precision
-	const pi = calculatePi(iterations);
+    for (let n = 2; n <= iterations * 2; n += 2) {
+      const temp = (4 / (n * (n + 1) * (n + 2))) * x;
+      pi += temp;
+      x = -x;
+    }
+    return pi;
+  }
 
-	res.json({
-		pi: pi.toString(),
-	});
-});
+  app.get("/pi", async (req, res) => {
+    iterations += 1000; // increase for next precision
+    const pi = calculatePi(iterations);
 
-app.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`);
-});
+    await redis.set("pi", pi.toString());
+
+    res.json({
+      pi: pi.toString(),
+    });
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+})();
