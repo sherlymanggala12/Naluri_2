7eb6025839cb9db45209a2b7120031d0d723d7ce
Sherly Manggala <v.sherly.manggala@adira.co.id>
2025-10-19 14:21:22 +0700

    changes background colour, adding stars at the back, changed pi and circumference text style

--- App/app/index.tsx
+++ App/app/index.tsx
@@ -10,16 +10,25 @@ import {
 	ImageStyle,
 } from "react-native";
 import axios from "axios";
+import {LinearGradient} from "expo-linear-gradient";
 
 interface PiResponse {
 	pi: string;
 }
 
-const {width} = Dimensions.get("window");
+const {width, height} = Dimensions.get("window");
+
+const generateStars = (count = 50) => {
+	return Array.from({length: count}).map(() => ({
+		left: Math.random() * width,
+		top: Math.random() * height,
+		size: Math.random() * 2 + 1,
+		delay: Math.random() * 4000,
+	}));
+};
 
 export default function Index() {
-	const [pi, setPi] = useState<number | null>(3.14); // using default value of 3.14 for pi
-
+	const [pi, setPi] = useState<number | null>(3.14);
 	const spinAnimation = useRef(new Animated.Value(0)).current;
 
 	useEffect(() => {
@@ -45,6 +54,30 @@ export default function Index() {
 		outputRange: ["0deg", "360deg"],
 	});
 
+	const stars = useRef(generateStars(70)).current;
+	const opacities = useRef(stars.map(() => new Animated.Value(0))).current;
+
+	useEffect(() => {
+		opacities.forEach((opacity, index) => {
+			const fade = () => {
+				Animated.sequence([
+					Animated.timing(opacity, {
+						toValue: 1,
+						duration: 1500,
+						delay: stars[index].delay,
+						useNativeDriver: true,
+					}),
+					Animated.timing(opacity, {
+						toValue: 0,
+						duration: 1500,
+						useNativeDriver: true,
+					}),
+				]).start(() => fade());
+			};
+			fade();
+		});
+	}, []);
+
 	const fetchData = async () => {
 		try {
 			const response = await axios.get<PiResponse>("http://localhost:3000/pi");
@@ -56,24 +89,47 @@ export default function Index() {
 
 	const calculateCircumference = () => {
 		const radius = 695700;
-		return pi ? 2 * pi * radius : 0;
+		return pi ? (2 * pi * radius).toString() : "0";
 	};
 
 	return (
-		<View style={$container}>
+		<LinearGradient
+			colors={["#020E1C", "#0A1F30"]}
+			start={{x: 0, y: 0}}
+			end={{x: 1, y: 1}}
+			style={$container}
+		>
+			{stars.map((star, index) => (
+				<Animated.View
+					key={index}
+					style={[
+						$star,
+						{
+							left: star.left,
+							top: star.top,
+							width: star.size,
+							height: star.size,
+							opacity: opacities[index],
+						},
+					]}
+				/>
+			))}
 			<View style={$textContainer}>
 				<Text style={$text}>
-					With our latest calculation of <br/>pi = {pi},<br /><br />
-          did you know that our sun's circumference is {calculateCircumference()} km?
+					With our latest calculation of {"\n"}
+					<Text style={$textOrange}>pi = {pi}</Text>,{"\n\n"}
+					did you know that our sun's circumference is {"\n"}
+					<Text style={$textOrange}>{calculateCircumference()} km</Text>?
 				</Text>
 			</View>
+
 			<View style={$imageContainer}>
 				<Animated.Image
 					source={require("../assets/images/sun.png")}
 					style={[$sun, {transform: [{rotate: spin}]}]}
 				/>
 			</View>
-		</View>
+		</LinearGradient>
 	);
 }
 
@@ -81,33 +137,46 @@ const $container: ViewStyle = {
 	flex: 2,
 	justifyContent: "center",
 	alignItems: "center",
-	backgroundColor: "#00111a",
 	flexDirection: width > 768 ? "row" : "column",
 	overflow: "hidden",
-	padding: 30,
+	padding: width * 0.2,
 };
 
 const $textContainer: ViewStyle = {
 	flex: width > 768 ? 1 : 0.5,
 	justifyContent: "flex-end",
 	alignItems: "flex-end",
+	zIndex: 2,
 };
 
 const $text: TextStyle = {
-	fontFamily: "'Nunito Sans', sans-serif",
+	fontFamily: "Lato, sans-serif",
 	color: "#ffffff",
 	fontSize: width > 768 ? 24 : 18,
 	textAlign: "center",
+};
+
+const $textOrange: TextStyle = {
+	color: "#FFA500",
+	fontSize: width > 768 ? 28 : 22,
 };
 
 const $imageContainer: ViewStyle = {
 	flex: width > 768 ? 1 : 1.5,
 	justifyContent: "flex-start",
 	alignItems: "flex-start",
+	zIndex: 2,
 };
 
 const $sun: ImageStyle = {
 	width: width * (width > 768 ? 0.3 : 0.5),
 	height: width * (width > 768 ? 0.3 : 0.5),
-	...(width > 768 ? {marginLeft: 20} : {marginTop: 20}),
+	...(width > 768 ? {marginLeft: width * 0.1} : {marginTop: width * 0.1}),
+};
+
+const $star: ViewStyle = {
+	position: "absolute",
+	backgroundColor: "white",
+	borderRadius: 10,
+	zIndex: 1,
 };
